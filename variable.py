ae41bce3f7976cc7bd357ffbed5eaa494daeb92e
daruvurisowmya <109509763+daruvurisowmya@users.noreply.github.com>
2022-07-22 14:37:25 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,45 @@
+# a=6
+# number=8
+# _=9
+# print(_)
+# print(a)
+# print(number) 
+# print(h) # NameError: name 'h' is not defined
+
+# _com5=7
+# _rt_=3
+#6t=7 #syntax error
+# #a b=5 # syntax error
+# c_r=7
+# abcdefghijklmnopqrstuvwxyz=4
+
+# A=5;B=7
+# print(A)
+# print(B)
+# C=A
+# A=B
+# print(C)
+# print(A)
+
+# A=5;B=7
+# print(A)
+# print(B)
+# C=A
+# A=B
+# B=C
+# print(A)
+# print(B)
+# print(C)
+
+# x=6 ; y=8
+# print(x) ; print(y)
+# x,y=y,x
+# print(x) ; print(y)_
+
+# print('abc'.isidentifier())
+# print('a b c'.isidentifier())
+# print('_'.isidentifier())
+# print('a_b_'.isidentifier())
+# print('a3b_'.isidentifier())
+# print('a3b_7'.isidentifier())
+# print('43b_7'.isidentifier())
